347f10a5616d1edba03224a0e24c3c09f803f02e
IshaGupta18 <ishagupta1828@gmail.com>
2019-05-08 20:16:36 +0530

    adding comments

--- parsing.js
+++ parsing.js
@@ -1,3 +1,4 @@
+//determines headers for columns
 function determineHeaders(mat){
 	var headers=[]
 	var flag=false;
@@ -19,6 +20,7 @@ function determineHeaders(mat){
 			}
 		}
 	}
+	//if there are no headers present, make dummy header names
 	if (flag && headers.length!=mat[0][0].length){
 		for (var i=0;i<mat[0][0].length;i++){
 			headers[i]="Column"+(i+1);
@@ -27,6 +29,7 @@ function determineHeaders(mat){
 	console.log(headers);
 
 }
+//using papaparse, parsing the retrieved file
 function parse(file){
 	var mat=[];
 	var count=0;
@@ -40,20 +43,24 @@ function parse(file){
 		},
 		complete: function() {
 			console.log(mat);
+			//calling a function to determine headers for columns
 			determineHeaders(mat);
 		}
 	});
 	
 	
 }
+//extracts the file uploaded in the field
 function handleFileSelectlocal(evt) {
 	var csv_file_local = evt.target.files[0];
 	parse(csv_file_local);
 }
+//reads the input from the text field in which the link of the remote file is included
 function handleFileSelectremote(val){
 	var csv_file_remote = val;
 	parse(csv_file_remote);
 }
+//this triggers handleFileSelectLocal function whenever a file is uploaded in the field
 $(document).ready(function(){
 	$(".csv_file").change(handleFileSelectlocal);
 });
