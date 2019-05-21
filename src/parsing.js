649120cff7bb3c682d55bb6e009b504682cc7b04
IshaGupta18 <ishagupta1828@gmail.com>
2019-05-21 17:42:13 +0530

    Adding CSV string import function

--- src/parsing.js
+++ src/parsing.js
@@ -1,10 +1,28 @@
+var counting=0;
+function saveAsImage(){
+	console.log("at image");
+	var x=new Date();
+	var timestamp=x.getTime();
+	$("#save_as_image").click(function() {
+	  $("#canvas").get(0).toBlob(function(blob) {
+	    saveAs(blob, "chart"+timestamp);
+	  });
+	});
+}
+function colorBackground(ctx,canv){
+	Chart.plugins.register({
+	    beforeDraw: function() {
+	        ctx.fillStyle = 'white';
+	        ctx.fillRect(0, 0, canv.width, canv.height);
+	    }
+	});
+}
 function determineType(type){
+	console.log("at type");
 	if (type=="Basic" || type=="Stepped" || type=="Point"){
-		console.log("wtf");
 		return 'line';
 	}
 	else if (type=="Horizontal"){
-		console.log("did i enter");
 		return 'horizontalBar';
 	}
 	else if (type=="Vertical"){
@@ -15,21 +33,31 @@ function determineType(type){
 		return type.toLowerCase();
 	}
 }
-function determineConfig(type){
+function determineData(type,i,hash){
+	console.log("at data");
+	h={};
 	if (type=="Basic"){
-		return {'fill': false};
+		h['fill'] = false;
 	}
 	else if (type=="Stepped"){
-		return {'steppedLine': true, 'fill': false};
+		h['steppedLine']= true;
+		h['fill']= false;
 	}
 	else if (type=="Point"){
-		return {'showLine': false, 'pointRadius': 10};
+		h['showLine']= false;
+		h['pointRadius']= 10;
 	}
-	else{
-		return {};
-	}
+	h['backgroundColor']=colorGenerator(i,"bg",type,hash['y_axis_values'+i].length);
+	h['borderColor']=colorGenerator(i,"bo",type,hash['y_axis_values'+i].length);
+	h['borderWidth']=1;
+	h['label']=hash['labels'][1][i];
+	h['data']=hash['y_axis_values'+i];
+	return h;
+	
 }
 function graphMenu(){
+	console.log("at menu");
+	document.getElementById("graph_menu").innerHTML="";
 	var bar=["Bar","Horizontal","Vertical"];
 	var line=["Line","Basic","Stepped","Point"];
 	var disc=["Disc","Pie","Doughnut","Radar"];
@@ -54,6 +82,7 @@ function graphMenu(){
 	}
 }
 function colorGenerator(i,tb,type,count){
+	console.log("at color");
 	var colors=['rgba(255, 77, 210, 0.5)','rgba(0, 204, 255, 0.5)','rgba(128, 0, 255, 0.5)','rgba(255, 77, 77, 0.5)','rgba(0, 179, 0, 0.5)','rgba(255, 255, 0, 0.5)','rgba(255, 0, 102, 0.5)','rgba(0, 115, 230, 0.5)'];
 	var bordercolors=['rgb(255, 0, 191)','rgb(0, 184, 230)','rgb(115, 0, 230)','rgb(255, 51, 51)','rgb(0, 153, 0)','rgb(230, 230, 0)','rgb(230, 0, 92)','rgb(0, 102, 204)'];
 	var length=8;
@@ -81,6 +110,7 @@ function colorGenerator(i,tb,type,count){
 	}
 }
 function scales(hash){
+	console.log("at scales");
 	var scales= {
 		xAxes: [{
 			display: true,
@@ -99,35 +129,60 @@ function scales(hash){
 	}
 	return scales;
 }
-function plotGraph(hash,length,type){
+function determineConfig(hash,length,type){
+	console.log("at config");
 	var config = {};
 	config['type']=determineType(type);
 	var data={};
 	data['labels']=hash['x_axis_labels'];
 	var datasets=[];
 	for (var i=0;i<length;i++){
-		var h=determineConfig(type);
-		h['backgroundColor']=colorGenerator(i,"bg",type,hash['y_axis_values'+i].length);
-		h['borderColor']=colorGenerator(i,"bo",type,hash['y_axis_values'+i].length);
-		h['borderWidth']=1;
-		h['label']=hash['labels'][1][i];
-		h['data']=hash['y_axis_values'+i];
+		var h=determineData(type,i,hash);
 		datasets.push(h);
 	}
-	var options={'responsive':true, 'maintainAspectRatio': true};
+	var options={'responsive':true, 'maintainAspectRatio': true, 'chartArea': {
+					backgroundColor: 'rgb(204, 102, 255)'
+				}};
 	options['scales']=scales(hash);
 	config['options']=options;
 	data['datasets']=datasets;
 	config['data']=data;
-	document.getElementById('canvas').remove();
-	var canv=document.createElement('canvas');
-	canv.id="canvas";
-	document.getElementById('canvas_container').appendChild(canv);
-	var ctx = document.getElementById('canvas').getContext('2d');
-	window.myLine = new Chart(ctx, config);
+	return config;
 }
-function afterSampleData(completeData,headers){
-	document.getElementById("upload").onclick = function(e){
+function plotGraph(hash,length,type,ci,flag){
+	if (flag){
+	console.log("at plotGraph");
+	document.getElementById('canvas_container').innerHTML="";}
+	
+	var div = document.createElement('div');
+	div.classList.add('chart-container');
+
+	console.log(ci+"i");
+	var canv=document.createElement('canvas');
+	// canv.id="canvas"+ci;
+	div.appendChild(canv);
+	document.getElementById('canvas_container').appendChild(div);
+	var ctx = canv.getContext('2d');
+	colorBackground(ctx,canv);
+	new Chart(ctx, determineConfig(hash,length,type));
+	$('.carousel').carousel(2);
+	// saveAsImage();
+	// new RangeSliderChart({
+
+	// 	chartData: config, //The same data you give to Chart.js
+	// 	chartOpts: options, //Your Chart.js options
+	// 	chartType: type, //Which Chart.js chart you want (eg. Lie, Bar, Pie, etc.)
+	// 	chartCTX: ctx, //your canvas context
+
+	// 	class: 'my-chart-ranger', //Specifies a custom class you want applied to your sliders
+
+	// 	initial: [3, 10] //Which data points to start the sliders on
+	// })
+}
+function afterSampleData(completeData,headers,ci,flag){
+	console.log("at checkbox");
+	document.getElementById("plot_graph").onclick = function(e){
+		console.log("at click on plot_graph");
 		e.preventDefault();
 		var hash={};
 		var ix=$('input[name=x_axis_column]:checked').val();
@@ -143,31 +198,40 @@ function afterSampleData(completeData,headers){
 		}
 		var labels=[headers[ix],y_axis_names];
 		hash["labels"]=labels;
-		var type=$('input[name=types]:checked').val()
-		console.log(type);
+		var type=$('input[name=types]:checked').val();
 		console.log(hash);
-		plotGraph(hash,columns.length,type);
+		plotGraph(hash,columns.length,type,ci,flag);
+
 	};
 
 }
-function tableGenerator(sampleData,headers,name,tableName,typeOfInput,validValues,completeData){
+function tableGenerator(sampleData,headers,name,tableName,typeOfInput,validValues,ci,flag,completeData,tableType,badgeType){
+	console.log("at tableGenerator");
 	document.getElementById(tableName).innerHTML="";
 	var trhead=document.createElement('tr');
 	for (var i=0;i<headers.length;i++){
 		var td=document.createElement('td');
-		td.appendChild(document.createTextNode(headers[i]));
+		var span=document.createElement('span');
+		var textnode=document.createTextNode(headers[i]);
+		span.appendChild(textnode);
+		span.classList.add("badge");
+		span.classList.add("badge-pill");
+		span.classList.add(badgeType);
+		td.appendChild(span);
 		for (var j=0;j<validValues.length;j++){
 			if (validValues[j]==headers[i]){
 			var checkbox=document.createElement('input')
 			checkbox.type = typeOfInput;
 			checkbox.value = i;
 			checkbox.name = name;
-			td.appendChild(checkbox);}
+			checkbox.classList.add("check-inputs");
+			span.appendChild(checkbox);}
 		}
 		trhead.appendChild(td);
 	}
+	trhead.classList.add(tableType);
 	document.getElementById(tableName).appendChild(trhead);
-	for(var i=0;i<5;i++){
+	for(var i=0;i<sampleData[0].length;i++){
 		var tr=document.createElement('tr');
 		for(var j=0;j<headers.length;j++){
 			var td=document.createElement('td');
@@ -176,17 +240,27 @@ function tableGenerator(sampleData,headers,name,tableName,typeOfInput,validValue
 		}
 		document.getElementById(tableName).appendChild(tr);
 	}
-	afterSampleData(completeData,headers);
-
+	
+	
+	afterSampleData(completeData,headers,ci,flag);
 
 }
 function sampleDataXandY(sampleData,headers,validForYAxis,completeData){
-	tableGenerator(sampleData,headers,'x_axis_column','tablex','radio',headers,completeData);
-	tableGenerator(sampleData,headers,'y_axis_column','tabley','checkbox',validForYAxis,completeData);
+	console.log("at sampleDataXandY");
+	document.getElementById("add_graph").onclick = function(e){
+		console.log("at add_graph");
+		counting++;
+		$('.carousel').carousel(1);
+		tableGenerator(sampleData,headers,'x_axis_column','tablex','radio',headers,counting,false,completeData,'table-success','badge-success');
+		tableGenerator(sampleData,headers,'y_axis_column','tabley','checkbox',validForYAxis,counting,false,completeData,'table-warning','badge-warning');
+		graphMenu();
+		
+	}
+	tableGenerator(sampleData,headers,'x_axis_column','tablex','radio',headers,0,true,completeData,'table-success','badge-success');
+	tableGenerator(sampleData,headers,'y_axis_column','tabley','checkbox',validForYAxis,0,true,completeData,'table-warning','badge-warning');
 	graphMenu();
 
 }
-
 //preparing sample data for the user to choose the columns from
 function extractSampleData(completeData,headers){
 	var sampleData=[];
@@ -194,11 +268,16 @@ function extractSampleData(completeData,headers){
 	for (var i=0;i<headers.length;i++){
 		sampleData[i]=[];
 	}
+	var maxval=5;
+	if (completeData.length<5){
+		maxval=completeData.length;
+	}
 	for (var i=0;i<headers.length;i++){
 		var counter=0;
+
 		var bool=false;
 		for(var j=0;j<completeData[i].length;j++){
-			if (counter>=5){
+			if (counter>=maxval){
 				break;
 			}
 			else if (completeData[i][j]!=null || completeData[i][j]!=undefined){
@@ -212,7 +291,7 @@ function extractSampleData(completeData,headers){
 			validForYAxis.push(headers[i]);
 		}
 	}
-	console.log(sampleData);
+	console.log(sampleData,"sampleData");
 	console.log(validForYAxis);
 	sampleDataXandY(sampleData,headers,validForYAxis,completeData);
 
@@ -283,12 +362,21 @@ function parse(file){
 		},
 		complete: function() {
 			console.log(mat);
+			$('.carousel').carousel(1);
 			//calling a function to determine headers for columns
 			determineHeaders(mat);
 		}
+	});	
+	
+}
+
+function parse_string(stringVal){
+	var values = Papa.parse(stringVal,{
+		dynamicTyping: true,
+		comments: true
 	});
-	
-	
+	console.log(values);
+
 }
 //extracts the file uploaded in the field
 function handleFileSelectlocal(evt) {
@@ -297,18 +385,74 @@ function handleFileSelectlocal(evt) {
 		alert("Invalid file type");
 	}
 	else{
-		parse(csv_file_local);}
+		$('.drag_drop_heading').text(csv_file_local['name']);
+		document.getElementById("upload").onclick = function(e){
+			parse(csv_file_local);
+		}
+
+	}
 }
 //reads the input from the text field in which the link of the remote file is included
 function handleFileSelectremote(val){
 	var csv_file_remote = val;
-	parse(csv_file_remote);
+	var l=csv_file_remote.length;
+	if (csv_file_remote.slice(l-3,l)!="csv"){
+		alert("Invalid URL");
+	}
+	else{
+		document.getElementById("upload").onclick = function(e){
+
+			parse(csv_file_remote);
+		}
+	}
+}
+function handleFileSelectstring(val){
+	var csv_string = val.split("\n");
+	var mat=[];
+	for (var i=0;i<csv_string.length;i++){
+		if (csv_string[i]=="" || csv_string[i]==" "){
+			continue;
+		}
+		var dataHash=Papa.parse(csv_string[i],{
+			dynamicTyping: true,
+			comments: true
+		});
+		mat[i]=dataHash['data'][0];
+	}
+	console.log(mat);
+	$('.carousel').carousel(1);
+	determineHeaders(mat);
 }
 //this triggers handleFileSelectLocal function whenever a file is uploaded in the field
 $(document).ready(function(){
+
 	$(".csv_file").change(handleFileSelectlocal);
 	$(".remote_file").on('change',function(){
 		handleFileSelectremote(this.value);
 	});
+	$(".csv_string").on('change',function(){
+		handleFileSelectstring(this.value);
+	});
+
+});
+document.getElementById("update_graph").onclick = function(e){
+	$('.carousel').carousel(1);
+}
+$('.carousel').carousel({
+    interval: false
+});
+$('.xytoggle').bootstrapToggle({
+	on: 'X-Axis',
+  	off: 'Y-Axis'
 });
 
+
+$('input[name=xy]:checked').change(function(){
+	var ixy=$('input[name=xy]:checked').val();
+	var ixx=0;
+	if (ixy==undefined){
+		ixx=1;
+	}
+	$('#xtable').toggle( ixx===0);
+	$('#ytable').toggle( ixx===1);
+});
